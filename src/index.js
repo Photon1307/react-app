45781de540e1fc8cab21bab8ae2d2774d6ef8404
Avery Lin <averylin@Averys-MacBook-Pro.local>
2022-11-02 19:33:38 +0800

    feat: A

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import Sketch from 'react-p5';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+	y = 0;
+	direction = '^';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	setup = (p5, parentRef) => {
+		p5.createCanvas(200, 200).parent(parentRef);
+	};
+
+	draw = (p5) => {
+		p5.background(0);
+		p5.fill(255, this.y * 1.3, 0);
+		p5.ellipse(p5.width / 2, this.y, 50);
+		if (this.y > p5.height) this.direction = '';
+		if (this.y < 0) {
+			this.direction = '^';
+		}
+		if (this.direction === '^') this.y += 8;
+		else this.y -= 4;
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1>react-p5</h1>
+				<Sketch setup={this.setup} draw={this.draw} />
+			</div>
+		);
+	}
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
